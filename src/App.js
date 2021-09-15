212f0cd13da4585f31262dc7ee1b037e85d81087
Jinok <joseo46@hotmail.com>
2021-09-15 20:25:22 +1000

    add comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import PreviewContent from './components/PreviewContent';
 import Btn from './components/Btn';
+import PreviewContent from './components/PreviewContent';
 import sedanIcon from './images/icon-sedans.svg';
 import suvsIcon from './images/icon-suvs.svg';
 import luxuryIcon from './images/icon-luxury.svg';
@@ -18,8 +18,9 @@ function App() {
               alt='icon-sedan'
               icon={sedanIcon}
               title='sedans'
-              content='Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
-  or on your next road trip.'
+              content='Choose a sedan for its affordability and 
+              excellent fuel economy. Ideal for cruising in the city 
+              or on your next road trip.'
             >
               <Btn title='Learn More' textColor='text-honey' />
             </PreviewContent>
@@ -29,8 +30,9 @@ function App() {
               alt='icon-sedan'
               icon={suvsIcon}
               title='SUVs'
-              content='Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
-  and off-road adventures.'
+              content='Take an SUV for its spacious interior, 
+              power, and versatility. Perfect for your next family 
+              vacation and off-road adventures.'
             >
               <Btn title='Learn More' textColor='text-forest' />
             </PreviewContent>
@@ -40,8 +42,9 @@ function App() {
               alt='icon-sedan'
               icon={luxuryIcon}
               title='Luxury'
-              content='Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
-  rental and arrive in style.'
+              content='Cruise in the best car brands without 
+              the bloated prices. Enjoy the enhanced comfort of 
+              a luxury rental and arrive in style.'
             >
               <Btn title='Learn More' textColor='text-darkforest' />
             </PreviewContent>
