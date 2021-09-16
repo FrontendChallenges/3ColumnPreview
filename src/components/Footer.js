b019eae527ab02a15cca3cf182bbb3550b63618f
Jinok <joseo46@hotmail.com>
2021-09-16 15:09:59 +1000

    modify footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row';
 
 function Footer() {
   return (
     <footer className='text-center py-5'>
       <span>Challenge by </span>
-      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+      <a
+        href='https://www.frontendmentor.io?ref=challenge'
+        rel='noreferrer'
+        target='_blank'
+      >
         Frontend Mentor
       </a>
-      <span>. Coded by </span>
-      <a href='#'>Jinok</a>.
+      <span>. Coded by</span>
+      <a href='#top'>Jinok</a>.
     </footer>
   );
 }
