9a7b24884b54211c0be18197dbe4f8ec257d5719
Jinok <joseo46@hotmail.com>
2021-09-16 13:29:51 +1000

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+
+function Footer() {
+  return (
+    <footer className='text-center py-5'>
+      <span>Challenge by </span>
+      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+        Frontend Mentor
+      </a>
+      <span>. Coded by </span>
+      <a href='#'>Jinok</a>.
+    </footer>
+  );
+}
+
+export default Footer;
